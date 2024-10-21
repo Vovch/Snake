92791e793d75664988a118749d5fbde0e4f0c3bc
vovch00 <vovch00@gmail.com>
2024-10-21 08:00:47 +0000

    fix versions

--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package com.vovch.supersnake
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'snake_game.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SuperSnake());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SuperSnake extends StatelessWidget {
+  const SuperSnake({super.key});
 
   @override
   Widget build(BuildContext context) {
